cd2202f3a12fd5103a317afbb5bee93a133bf515
Diksh Sawaliya <80209884+dikshsawaliya@users.noreply.github.com>
2024-05-07 02:11:51 +0530

    Added the Main Activity in which the first value state text field is added

---
+++ app/src/main/java/com/diksh/firstcompose/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.diksh.firstcompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.diksh.firstcompose.ui.theme.FirstComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            displayName("Reaper")
+        }   
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Preview(showBackground = true, widthDp = 300, heightDp = 500)
+    @Composable
+    fun displayName(name : String = "Diksh"){
+        val state = remember {mutableStateOf("")}
+        TextField(value = state.value,
+            onValueChange ={
+                state.value = it
+            },
+            label = { Text(text = "Enter Name",
+                            color = Color.Blue)})
+    }
+}
